7cdb1d0a100ca95c43456bd1304efd389c53f4fd
Jack <jpntol@gmail.com>
2024-10-17 01:03:23 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,84 @@
 import pandas as pd
-import os
 
-def clean_food_nutrient():
-    df = pd.read_csv('food_nutrient.csv', low_memory=False)
-    food_nutrient_df_cleaned = df[['fdc_id', 'nutrient_id', 'amount']].rename(columns={'fdc_id': 'food_id'})
-    food_nutrient_df_cleaned['food_id'] = pd.to_numeric(food_nutrient_df_cleaned['food_id'], errors='coerce')
-    food_nutrient_df_cleaned = food_nutrient_df_cleaned.sort_values(by='food_id', ascending=True)
-    return food_nutrient_df_cleaned
+def read_and_clean_branded_food(file_path):
+    branded_food_df = pd.read_csv(file_path, low_memory=False)
+    columns_to_drop = [
+        "subbrand_name", "brand_owner", "brand_name", "gtin_upc",
+        "not_a_significant_source_of", "household_serving_fulltext",
+        "branded_food_category", "data_source", "package_weight",
+        "modified_date", "available_date", "market_country",
+        "discontinued_date", "preparation_state_code", "trade_channel",
+        "short_description"
+    ]
+    branded_food_df.drop(columns=columns_to_drop, inplace=True)
+    branded_food_df['food_serving_size'] = branded_food_df['serving_size'].astype(str) + ' ' + branded_food_df['serving_size_unit']
+    branded_food_df.drop(columns=['serving_size', 'serving_size_unit'], inplace=True)
+    branded_food_df.rename(columns={'fdc_id': 'food_id'}, inplace=True)
+    return branded_food_df
 
-def clean_food():
-    df = pd.read_csv('food.csv', low_memory=False)
-    food_df_cleaned = df[['fdc_id', 'description']].rename(columns={'fdc_id': 'food_id', 'description': 'food_name'})
-    food_df_cleaned = food_df_cleaned.sort_values(by='food_id', ascending=True)
-    return food_df_cleaned
+def merge_branded_food_with_food_data(branded_food_df, food_file_path):
+    food_df = pd.read_csv(food_file_path)
+    merged_branded_food_df = pd.merge(branded_food_df, food_df[['fdc_id', 'description']], left_on='food_id', right_on='fdc_id', how='left')
+    merged_branded_food_df.rename(columns={'description': 'food_name'}, inplace=True)
+    merged_branded_food_df.drop(columns=['fdc_id'], inplace=True)
+    merged_branded_food_df.rename(columns={'ingredients': 'food_ingredients'}, inplace=True)
+    merged_branded_food_df = merged_branded_food_df[['food_id', 'food_name', 'food_ingredients', 'food_serving_size']]
+    return merged_branded_food_df
 
-def clean_nutrient():
-    df = pd.read_csv('nutrient.csv', low_memory=False)
-    nutrient_df_cleaned = df[['id', 'name', 'unit_name']].rename(columns={'id': 'nutrient_id', 'name': 'nutrient_name', 'unit_name': 'nutrient_unit'})
-    nutrient_df_cleaned['nutrient_id'] = pd.to_numeric(nutrient_df_cleaned['nutrient_id'], errors='coerce')
-    nutrient_df_cleaned = nutrient_df_cleaned.sort_values(by='nutrient_id', ascending=True)
-    return nutrient_df_cleaned
+def clean_nutrient(file_path):
+    nutrient_df = pd.read_csv(file_path, low_memory=False)
+    cleaned_nutrient_df = nutrient_df[['id', 'name', 'unit_name']].rename(
+        columns={'id': 'nutrient_id', 'name': 'nutrient_name', 'unit_name': 'nutrient_unit'}
+    )
+    cleaned_nutrient_df['nutrient_id'] = pd.to_numeric(cleaned_nutrient_df['nutrient_id'], errors='coerce')
+    cleaned_nutrient_df = cleaned_nutrient_df.sort_values(by='nutrient_id', ascending=True)
+    return cleaned_nutrient_df
 
-def merge_food_nutrient_with_nutrient_information(food_nutrient_df_cleaned, nutrient_df_cleaned):
-    merged_df = pd.merge(food_nutrient_df_cleaned, nutrient_df_cleaned, on='nutrient_id')
-    merged_df['nutrient_amount'] = merged_df['amount'].astype(str) + " " + merged_df['nutrient_unit']
-    merged_food_nutrient_df = merged_df[['food_id', 'nutrient_name', 'nutrient_amount']]
+def clean_food_nutrient(file_path):
+    food_nutrient_df = pd.read_csv(file_path, low_memory=False)
+    cleaned_food_nutrient_df = food_nutrient_df[['fdc_id', 'nutrient_id', 'amount']].rename(columns={'fdc_id': 'food_id'})
+    cleaned_food_nutrient_df['food_id'] = pd.to_numeric(cleaned_food_nutrient_df['food_id'], errors='coerce')
+    cleaned_food_nutrient_df = cleaned_food_nutrient_df.sort_values(by='food_id', ascending=True)
+    return cleaned_food_nutrient_df
+
+def merge_food_nutrient_with_nutrient_information(cleaned_food_nutrient_df, cleaned_nutrient_df):
+    merged_food_nutrient_df = pd.merge(cleaned_food_nutrient_df, cleaned_nutrient_df, on='nutrient_id')
+    merged_food_nutrient_df['nutrient_amount'] = merged_food_nutrient_df['amount'].astype(str) + " " + merged_food_nutrient_df['nutrient_unit']
+    merged_food_nutrient_df = merged_food_nutrient_df[['food_id', 'nutrient_name', 'nutrient_amount']]
     return merged_food_nutrient_df
 
 def pivot_food_nutrient_with_nutrient_information(merged_food_nutrient_df):
-    pivoted_food_nutrient_df = merged_food_nutrient_df.pivot_table(index='food_id', columns='nutrient_name', values='nutrient_amount', aggfunc='first')
-    pivoted_food_nutrient_df.reset_index(inplace=True)
-    return pivoted_food_nutrient_df
+    pivoted_nutrient_df = merged_food_nutrient_df.pivot_table(
+        index='food_id', columns='nutrient_name', values='nutrient_amount', aggfunc='first'
+    )
+    pivoted_nutrient_df.reset_index(inplace=True)
+    return pivoted_nutrient_df
 
-def merge_with_food_names(pivoted_food_nutrient_df, food_df_cleaned):
-    merged_data_with_food_names = pd.merge(pivoted_food_nutrient_df, food_df_cleaned[['food_id', 'food_name']], on='food_id', how='inner')
-    cols = ['food_id', 'food_name'] + [col for col in merged_data_with_food_names.columns if col not in ['food_id', 'food_name']]
-    final_food_nutrient_data = merged_data_with_food_names[cols]
-    return final_food_nutrient_data
-
-def clean_up(files):
-    for file in files:
-        if os.path.exists(file):
-            os.remove(file)
+def merge_final_data(merged_branded_food_df, pivoted_nutrient_df):
+    final_merged_df = pd.merge(merged_branded_food_df, pivoted_nutrient_df, on='food_id', how='left')
+    
+    # Convert all text columns to uppercase
+    final_merged_df = final_merged_df.apply(lambda x: x.str.upper() if x.dtype == "object" else x)
+    
+    # Convert column names to uppercase
+    final_merged_df.columns = [col.upper() for col in final_merged_df.columns]
+    
+    # Sort the final DataFrame by 'food_id' in ascending order
+    final_merged_df = final_merged_df.sort_values(by='FOOD_ID', ascending=True)
+    
+    return final_merged_df
 
 def main():
-    food_nutrient_df_cleaned = clean_food_nutrient()
-    food_df_cleaned = clean_food()
-    nutrient_df_cleaned = clean_nutrient()
+    branded_food_df = read_and_clean_branded_food("branded_food.csv")
+    merged_branded_food_df = merge_branded_food_with_food_data(branded_food_df, "food.csv")
+    cleaned_nutrient_df = clean_nutrient("nutrient.csv")
+    cleaned_food_nutrient_df = clean_food_nutrient("food_nutrient.csv")
+    merged_food_nutrient_df = merge_food_nutrient_with_nutrient_information(cleaned_food_nutrient_df, cleaned_nutrient_df)
+    pivoted_nutrient_df = pivot_food_nutrient_with_nutrient_information(merged_food_nutrient_df)
+    final_df = merge_final_data(merged_branded_food_df, pivoted_nutrient_df)
     
-    merged_food_nutrient_df = merge_food_nutrient_with_nutrient_information(food_nutrient_df_cleaned, nutrient_df_cleaned)
-    pivoted_food_nutrient_df = pivot_food_nutrient_with_nutrient_information(merged_food_nutrient_df)
-    final_food_nutrient_data = merge_with_food_names(pivoted_food_nutrient_df, food_df_cleaned)
-    
-    final_food_nutrient_data_cleaned = final_food_nutrient_data.dropna(subset=['food_id', 'food_name'])
-
-    final_food_nutrient_data_cleaned.to_csv('fda_food_nutrient_data.csv', index=False)
-    
-    clean_up(['food_nutrient.csv', 'food.csv', 'nutrient.csv'])
+    # Export the final DataFrame to a CSV file
+    final_df.to_csv("food_data_final.csv", index=False)
 
 if __name__ == "__main__":
     main()
